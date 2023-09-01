482ad2903c16ec9af88d7fe43ea4faca83a871c8
Kenneth Carnes <kennethmcarnes@gmail.com>
2023-09-01 17:42:22 -0500

    fixed accidently removed code

--- Get-CodeContent.ps1
+++ Get-CodeContent.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+    Retrieves and displays the directory structure and contents of specified files.
+
+.DESCRIPTION
+    This function outputs the directory structure of a specified root directory and
+    the content of files within specified target directories relative to the root.
+    The results are copied to the clipboard for easy sharing or saved to a file.
+
+.PARAMETER RootDirectory
+    The path to the root directory.
+
+.PARAMETER TargetDirectories
+    A list of directories relative to the root whose file contents will be displayed.
+
+.PARAMETER SaveToFile
+    Switch to indicate whether to save output to a file instead of copying to clipboard.
+
+.PARAMETER OutputFile
+    The file where the output should be saved. Only used if SaveToFile is specified.
+
+.EXAMPLE
+    Get-CodeContent -RootDirectory "C:\code\azure-swa" -TargetDirectories @("infra", ".github")
+#>
 function Get-CodeContent {
     param (
         [string]$RootDirectory = (Read-Host "Please enter the root directory path"),
@@ -42,3 +66,43 @@ function Get-CodeContent {
         Write-Error $_.Exception.Message
     }
 }
+
+function Get-FileContents {
+    param (
+        [string]$RootPath,
+        [string[]]$Directories
+    )
+
+    $contentArray = @()
+
+    foreach ($dir in $Directories) {
+        $fullDirPath = Join-Path $RootPath $dir.Trim()
+
+        if (-not (Test-Path $fullDirPath)) {
+            Write-Error "The directory ($dir) does not exist within the root directory."
+            continue
+        }
+
+        $files = Get-ChildItem -Path $fullDirPath -Recurse -File
+
+        foreach ($file in $files) {
+            try {
+                $relativePath = $file.FullName.Replace($RootPath, '').TrimStart('\')
+                $contentArray += ""
+                $contentArray += "--- Contents of $relativePath ---"
+                $contentArray += (Get-Content $file.FullName | ForEach-Object { "    $_" })
+                $contentArray += "--- End of $relativePath ---"
+            }
+            catch {
+                Write-Error "Could not read the contents of $file. Error: $_"
+                continue
+            }
+        }
+        
+    }
+
+    return $contentArray
+}
+
+# Call the main function
+Get-CodeContent
