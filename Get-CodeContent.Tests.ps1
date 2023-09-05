de402a0d4ef055f4f0fbc72887a0e07d3700c476
Kenneth Carnes <kennethmcarnes@gmail.com>
2023-09-04 19:26:17 -0500

    Refactor GitHub Actions workflow, and Get-CodeCOntent.Tests.ps1 to include both linting and unit tests

--- Get-CodeContent.Tests.ps1
+++ Get-CodeContent.Tests.ps1
@@ -1,11 +1,39 @@
+# Get-CodeContent.Tests.ps1
+
+# Use BeforeAll to set up preconditions for tests
+# Import the PowerShell script containing the function to be tested by dot-sourcing it.
+BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+# Test suite for Get-CodeContent
 Describe "Get-CodeContent" {
+
+    # Group for basic tests
     Context "Basic Tests" {
+
+        # Test: Should error for non-existing directory
         It "Should return an error for a non-existing directory" {
-            { Get-CodeContent -RootDirectory "C:\NonExistingDirectory" } | Should -Throw
+            { Get-CodeContent -RootDirectory "C:\NonExistingDirectory" -ErrorAction Stop } | Should -Throw "The specified root directory does not exist."
         }
 
+        # Test: Should not error for existing directory
         It "Should not throw an error for an existing directory" {
-            { Get-CodeContent -RootDirectory "C:\Test-RootDirectory" } | Should -Not -Throw
+            { Get-CodeContent -RootDirectory "C:\Test-RootDirectory" -ErrorAction Stop } | Should -Not -Throw
+        }
+    }
+
+    # Group for advanced tests
+    Context "Advanced Tests" {
+
+        # Test: Should throw an error if no target directories are specified and -AllDirectories is not set
+        It "Should throw an error for no target directories" {
+            { Get-CodeContent -RootDirectory "C:\Test-RootDirectory" -TargetDirectories @() -ErrorAction Stop } | Should -Throw "You must specify at least one target directory or use -AllDirectories switch."
+        }
+
+        # Test: Should not throw an error if -AllDirectories is set
+        It "Should not throw an error for -AllDirectories" {
+            { Get-CodeContent -RootDirectory "C:\Test-RootDirectory" -AllDirectories -ErrorAction Stop } | Should -Not -Throw
         }
     }
 }
